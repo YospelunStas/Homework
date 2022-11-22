e4e84121321d5017b8fc5088d6df45064a2815d8
Stanislav <stas2001.nikitin@yandex.ru>
2022-11-22 15:28:35 +0400

    Задача на нахождее большего и меньшего чисел

---
+++ Task_2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+Console.WriteLine("Введите первое число");
+string str = Console.ReadLine();
+int a = int.Parse(str);
+Console.WriteLine("Введите второе число");
+str = Console.ReadLine();
+int b = int.Parse(str);
+Console.WriteLine();
+if (a > b)
+{
+    Console.Write(a);
+    Console.WriteLine(" = max");
+    Console.Write(b);
+    Console.WriteLine(" = min");
+}
+else
+{
+    Console.Write(b);
+    Console.WriteLine(" = max");
+    Console.Write(a);
+    Console.WriteLine(" = min");
+}
