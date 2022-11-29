5dd51fe5e7296ae3a792c75266b3449adc24ce54
Stanislav <stas2001.nikitin@yandex.ru>
2022-11-29 06:38:34 +0400

    Задача на определение чётности числа

---
+++ Task_6/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.Write("Введите Ваше число: ");
+string str = Console.ReadLine();
+int N = int.Parse(str);
+Console.WriteLine();
+if (N % 2 == 0)
+{
+    Console.Write(N);
+    Console.WriteLine(" - чётное");
+}
+else
+{
+    Console.Write(N);
+    Console.WriteLine(" - нечётное");
+}
