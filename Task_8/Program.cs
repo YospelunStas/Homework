dd10f8195b724cb521420ec4d5f8d4c752c36df7
Stanislav <stas2001.nikitin@yandex.ru>
2022-11-29 07:05:34 +0400

    Задача на нахождение всех чётных чисел в промежутке

---
+++ Task_8/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write("Введите Ваше число: ");
+string str = Console.ReadLine();
+int N = int.Parse(str);
+int a = 1;
+Console.WriteLine();
+Console.Write(N);
+Console.Write(": ");
+while (a <= N)
+{
+    if (a % 2 == 0)
+    {
+        Console.Write(a + ", ");
+        a++;
+    }
+    else
+    {
+        a++;
+    }
+}
